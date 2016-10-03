4ec5b4243c9e181a2160ea3bd3f61584feaf2836
Vadym Prokopets <vadymBlablalbal>
2016-10-03 23:58:29 +0300

    done

---
+++ Aforg/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Aforg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(3, 2);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(371, 261);
+            this.videoSourcePlayer1.TabIndex = 0;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            this.videoSourcePlayer1.ParentChanged += new System.EventHandler(this.videoSourcePlayer1_ParentChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.toolTip1.SetToolTip(this.button1, "Let\'s begin\r\n");
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(380, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(197, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(299, 279);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(149, 41);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Save photo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "(*.jpg)|*.jpg";
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutoPopDelay = 5000;
+            this.toolTip1.InitialDelay = 250;
+            this.toolTip1.IsBalloon = true;
+            this.toolTip1.ReshowDelay = 100;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 332);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.videoSourcePlayer1);
+            this.Name = "Form1";
+            this.Text = "Saver";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Aforg/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using AForge.Video.VFW;
+using AForge.Vision;
+using System.Threading;
+
+namespace Aforg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        FilterInfoCollection videodevices;
+        VideoCaptureDevice videoSource;
+        Thread saver;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = true;
+            videoSource = new VideoCaptureDevice(videodevices[comboBox1.SelectedIndex].MonikerString);
+            comboBox1.Visible = true;
+            videoSourcePlayer1.VideoSource = videoSource;
+            videoSource.Start();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoSource!=null) videoSource.Stop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Stabilize();
+            videodevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            for (int i = 0; i < videodevices.Count; i++) comboBox1.Items.Add(videodevices[i].Name);
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void videoSourcePlayer1_ParentChanged(object sender, EventArgs e)
+        {
+            
+        }
+        Bitmap bmp;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bmp = videoSourcePlayer1.GetCurrentVideoFrame();
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            bmp.Save(saveFileDialog1.FileName);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Stabilize();
+        }
+
+
+        private void Stabilize()
+        {
+            button1.Left = this.ClientSize.Width / 2 - button1.Width;
+            button1.Top = this.ClientSize.Height - button1.Height - 10;
+
+            button2.Left = button1.Left + button1.Width + 10;
+            button2.Top = button1.Top;
+
+            comboBox1.Left = this.ClientSize.Width - comboBox1.Width - 10;
+            videoSourcePlayer1.Width = Math.Min(comboBox1.Left - 10, button1.Top - 10);
+            videoSourcePlayer1.Height = Math.Min(comboBox1.Left - 10, button1.Top - 10);
+        }
+    }
+}
